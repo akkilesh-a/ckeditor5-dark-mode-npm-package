533e3282f572c77580f67af8dcd85b24baea44ee
spacecentre <akkilalagar05@gmail.com>
2025-06-14 08:39:38 +0000

    refactor: make theme not optional

--- src/components/dark-mode-wrapper.tsx
+++ src/components/dark-mode-wrapper.tsx
@@ -6,7 +6,7 @@ const CKEditorDarkModeWrapper = ({
   theme,
 }: {
   children?: ReactNode;
-  theme?: string | undefined;
+  theme: string | undefined;
 }) => {
   return <div className={`${theme === "dark" ? "dark" : ""}`}>{children}</div>;
 };
